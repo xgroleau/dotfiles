ee3bcc5bbba80f3b22aa50294179b7add26ee4c8
Xavier Groleau <xavier@Jaj-Surface.localdomain>
2020-11-15 14:16:42 -0500

    Fixed updating via bootstrap

--- .config/yadm/scripts/config_installers/doom.sh
+++ .config/yadm/scripts/config_installers/doom.sh
@@ -2,6 +2,20 @@
 set -euo pipefail
 
 
-git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
-git -C ~/.emacs.d checkout develop #Just in case master becomes the default
-~/.emacs.d/bin/doom --yes install
+function doom_install {
+	git clone https://github.com/hlissner/doom-emacs ${HOME}/.emacs.d
+	git -C ${HOME}/.emacs.d checkout develop #Just in case master becomes the default
+	${HOME}/.emacs.d/bin/doom --yes install
+}
+
+function doom_update {
+	${HOME}/.emacs.d/bin/doom upgrade
+	${HOME}/.emacs.d/bin/doom sync -u
+}
+
+# if already install
+if [ -d "${HOME}/.emacs.d" ]; then
+	doom_update
+else 
+	doom_install
+fi
--- .config/yadm/scripts/config_installers/zsh.sh
+++ .config/yadm/scripts/config_installers/zsh.sh
@@ -1,15 +1,33 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-curl --proto '=https' --tlsv1.2 -sSfL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh > /tmp/oh-my-zsh
-chmod +x /tmp/oh-my-zsh
+function install_missing_plugin {
+	[ ! -d "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ] && \
+		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-#Executing oh-my-zsh
-export RUNZSH="no"
-/tmp/oh-my-zsh --keep-zshrc
+	[ ! -d "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ] && \
+		git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-#Installing zsh plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+	[ ! -d "${ZSH_CUSTOM:=${HOME}/.oh-my-zsh/custom}/plugins/zsh-completions" ] && \
+		git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=${HOME}/.oh-my-zsh/custom}/plugins/zsh-completions
+
+	[ ! -d "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/themes/powerlevel10k" ] && \
+		git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/themes/powerlevel10k
+}
+
+function zsh_install {
+	curl --proto '=https' --tlsv1.2 -sSfL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh > /tmp/oh-my-zsh
+	chmod +x /tmp/oh-my-zsh
+
+	#Executing oh-my-zsh
+	export RUNZSH="no"
+	/tmp/oh-my-zsh --keep-zshrc
+}
+
+
+# Already installed
+if [ ! -d "${HOME}/.oh-my-zsh" ]; then
+	zsh_install
+fi
+
+install_missing_plugin
