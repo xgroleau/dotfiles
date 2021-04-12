253b6715295df0faff92ee41addce9903f74a8fd
xgroleau <xavgroleau@gmail.com>
2021-04-12 10:10:39 -0400

    Added emacs everywhere

--- .config/yadm/scripts/packages/apt.sh
+++ .config/yadm/scripts/packages/apt.sh
@@ -8,5 +8,6 @@ packages=${packages/fd/fd-find}
 packages=${packages/network-manager-applet/network-manager-gnome}
 packages=${packages/xmllint/libxml2-utils}
 packages=${packages/picom/}
+packages=${packages/x11-tools/x11-utils}
 sudo apt-get update
 sudo apt-get -y install $packages
